f9c796425a28f3b46a5c09843ec88c9260b9b140
ctcncgr <ctc@ncgr.org>
2018-04-27 11:53:49 -0600

    better comments on stats calculation, removed some redundant logic

--- seqtools2/basic_fasta_stats.py
+++ seqtools2/basic_fasta_stats.py
@@ -27,7 +27,7 @@ parser.add_argument('--fasta', metavar = '</path/to/my/fasta.fa>',
 help='''FASTA file to filter, can be compressed''')
 
 parser.add_argument('--min_gap', metavar = '<INT>', default=10, type=int,
-help="""Minimum length of consecutive N's to consider a gap and create a scaffold""")
+help="""Minimum length of consecutive N's to consider a gap and create a scaffold (default: 10)""")
 
 parser.add_argument('--log_file', metavar = '<FILE>',
 default='./basic_fasta_stats.log',
@@ -52,25 +52,31 @@ logger = logging.getLogger('basic_fasta_stats')
 logger.addHandler(log_handler)
 
 
-def get_N50(contigs, total):
-    c = 0
-    n = 0
-    for l in reversed(contigs):
-        c += int(l)
-        n += 1
-        if c >= float(total)/2:
+def get_N50(lengths, total):
+    '''Calculates the N50 of the list lengths using a running sum and
+    
+       the provided total sum of lengths total
+    '''
+    total_length = 0  # will sum bases for all 
+    number = 0
+    for l in reversed(lengths):
+        total_length += int(l)
+        number += 1
+        if total_length >= float(total)/2:  # N50 is this length l
             return l
-    return 0
+    return 0  # if empty list
 
 
 def get_gc(gc, total):
+    '''Calculates the percentage gc'''
     if not total or not gc:
         return 0
-    pgc = float(gc)/int(total)
+    pgc = float(gc)/int(total)  # get percent GC
     return pgc
 
 
 def compile_metrics(metrics, lengths, bases):
+    '''Fill the metrics dictionary with the results from lengths and bases'''
     lengths['contigs'] = sorted(lengths['contigs'])
     lengths['gaps'] = sorted(lengths['gaps'])
     lengths['scaffolds'] = sorted(lengths['scaffolds'])
@@ -78,25 +84,29 @@ def compile_metrics(metrics, lengths, bases):
     if lengths['contigs']:
         metrics['maxcontig'] = lengths['contigs'][-1]
         metrics['mincontig'] = lengths['contigs'][0]
-        if metrics['contigbases']:
+        if metrics['contigbases']:  # contig N50
             metrics['contigN50'] = get_N50(lengths['contigs'], 
                                            metrics['contigbases'])
     if lengths['gaps']:
         metrics['maxgap'] = lengths['gaps'][-1]
         metrics['mingap'] = lengths['gaps'][0]
-        if metrics['gapbases']:
+        if metrics['gapbases']:  # gap N50
             metrics['gapN50'] = get_N50(lengths['gaps'], metrics['gapbases'])
     if lengths['scaffolds']:
         metrics['maxscaffold'] = lengths['scaffolds'][-1]
         metrics['minscaffold'] = lengths['scaffolds'][0]
-        if metrics['scaffoldbases']:
+        if metrics['scaffoldbases']:  # scaffold N50
             metrics['scaffoldN50'] = get_N50(lengths['scaffolds'], 
                                              metrics['scaffoldbases'])
     metrics['allbases'] = bases['total']
-    metrics['N50'] = get_N50(lengths['total'], bases['total'])
+    metrics['N50'] = get_N50(lengths['total'], bases['total'])  # N50 of all
 
 
 def basic_fasta_stats(fasta):
+    '''Main method for stats calculation.  Creates data structures
+
+       and controls workflow
+    '''
     if not fasta:  # Assume STDIN
         fasta = sys.stdin
     else:
@@ -112,7 +122,7 @@ def basic_fasta_stats(fasta):
                'maxcontig' : 0, 'mincontig' : 0,
                'contigbases' : 0, 'scaffoldbases' : 0, 'gapbases' : 0,
                'allbases' : 0, 'pgc' : 0}
-    min_gap = args.min_gap
+    min_gap = args.min_gap  # default 10
     lengths = {'scaffolds' : [], 'gaps' : [], 'contigs' : [], 'total' : []}
     scheck = 0
     gcheck = 0
@@ -122,8 +132,8 @@ def basic_fasta_stats(fasta):
     slen = 0
     length = 0
     i = 0
-    for record in get_seqio_record(fasta):
-        metrics['records'] += 1
+    for record in get_seqio_record(fasta):  # get records from SeqIO
+        metrics['records'] += 1  # increment total
         seq = record.seq
         if seq:
             seq = seq.upper()
@@ -131,9 +141,9 @@ def basic_fasta_stats(fasta):
             bases['total'] += length
             lengths['total'].append(length)
             while True:
-                i = seq.find('N', i)
+                i = seq.find('N', i)  # split all sequences into contigs
                 if i < 0:
-                    if not scheck:
+                    if not scheck:  # not a scaffold yet
                         lengths['contigs'].append(length)
                         metrics['contigs'] += 1
                         metrics['contigbases'] += length
@@ -142,7 +152,7 @@ def basic_fasta_stats(fasta):
                                 bases[b] += 1
                             else:
                                 bases['IUPAC'] += 1
-                    else:
+                    else:  # scaffold
                         clen = length - ccheck
                         lengths['contigs'].append(clen)
                         metrics['contigs'] += 1
@@ -153,12 +163,12 @@ def basic_fasta_stats(fasta):
                             else:
                                 bases['IUPAC'] += 1
                     break
-                gcheck = i
-                while i < length and seq[i] == 'N':
+                gcheck = i  # set position of gap start
+                while i < length and seq[i] == 'N':  # get gap lengths
                     i += 1
                 glen = i - gcheck
-                if glen >= min_gap:
-                    scheck = 1
+                if glen >= min_gap:  # check to see if this is a "gap"
+                    scheck = 1  # set check for scaffold
                     clen = gcheck - ccheck
                     lengths['gaps'].append(glen)
                     bases['N'] += glen
@@ -173,11 +183,11 @@ def basic_fasta_stats(fasta):
                         else:
                             bases['IUPAC'] += 1
                     ccheck = i
-            if scheck:
+            if scheck:  # if scaffold as set by gap checks above
                 metrics['scaffolds'] += 1
                 lengths['scaffolds'].append(length)
                 metrics['scaffoldbases'] += length
-        scheck = 0
+        scheck = 0  # reset all for the next sequence yielded
         gcheck = 0
         ccheck = 0
         glen = 0
