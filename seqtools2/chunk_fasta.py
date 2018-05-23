bdff06673228b5c55a46131a425a8ca1cc759836
ctcncgr <ctc@ncgr.org>
2018-05-23 13:01:54 -0600

    added byte chunking for FASTA files.  This allows you to split a reference for processing by SEQUENCE bytes

--- seqtools2/chunk_fasta.py
+++ seqtools2/chunk_fasta.py
@@ -30,6 +30,9 @@ parser.add_argument('--chunk_size', metavar = '<INT>', type=int,
 help='''Write N reads to file (default:1000)''',
 default=1000)
 
+parser.add_argument('--chunk_bytes', metavar = '<INT>', type=int,
+help='''Try to write N sequence bytes to file.  Keeps sequence intact''')
+
 parser.add_argument('--chunk_dir', metavar = '</path/to/chunks>',
 help='''Directory to write chunks in (default:./chunks)''',
 default='./chunks')
@@ -76,54 +79,60 @@ def write_chunk(record, chunk, gzip_me):
     chunk.write(output)
 
 
-def chunk_fasta(fasta, chunks, chunks_dir, gzip_me):
-    '''Chunk FASTA file.  Output files with chunks reads to chunks_dir'''
-    seqio_in = sys.stdin
-    fh = ''
+def process_filehandle(fh, chunks, chunks_dir, gzip_me, byte_chunks):
     count = 0
     total_reads = 0
     total_files = 1
     create_directories(os.path.abspath(chunks_dir))  # create chunks directory
     chunk = get_chunk(chunks_dir, total_files, gzip_me)
-    if not fasta:  # Check STDIN
-        logger.info('Parsing STDIN... Chunking reads in {}s...'.format(chunks))
-        for record in get_seqio_fasta_record(seqio_in):  # get SeqIO record
-            total_reads += 1
+    for record in get_seqio_fasta_record(fh):  # get SeqIO record
+        total_reads += 1
+        if byte_chunks:  # count is incremented by bytes of sequence
+            count += len(record.seq)  # bytes of current sequence
+        else:
             count += 1
-            if count > chunks:  # open new file close old file
-                count = 0
-                total_files += 1
-                chunk.close()
-                chunk = get_chunk(chunks_dir, total_files, gzip_me)
-                write_chunk(record, chunk, gzip_me)
-                continue  # dont write sequence twice
+        if count > chunks:  # open new file close old file
+            count = 0
+            total_files += 1
+            chunk.close()
+            chunk = get_chunk(chunks_dir, total_files, gzip_me)
             write_chunk(record, chunk, gzip_me)
-    else:  # Check FASTA
-        logger.info('Parsing FASTA file... Chunking reads in {}s...'.format(
-                                                                      chunks))
-        fh = return_filehandle(fasta)
-        for record in get_seqio_fasta_record(fh):  # Get SeqIO record
-            total_reads += 1
-            count += 1
-            if count > chunks:  # open new file close old file
-                count = 0
-                total_files += 1
-                chunk.close()
-                chunk = get_chunk(chunks_dir, total_files, gzip_me)
-                write_chunk(record, chunk, gzip_me)
-                continue  # don't write sequence twice
-            write_chunk(record, chunk, gzip_me)
+            continue  # dont write sequence twice
+        write_chunk(record, chunk, gzip_me)
     chunk.close()  # close last instance of chunk
     logger.info('Output {} reads in {} files {} at a time'.format(total_reads,
                                                                   total_files,
                                                                   chunks))
-            
+
+
+def chunk_fasta(fasta, chunks, chunks_dir, gzip_me, byte_chunks):
+    '''Chunk FASTA file.  Output files with chunks reads to chunks_dir
+    
+       if byte_chunks, chunk by bytes.  Will try to put chunks bytes in file.
+
+       Will not split sequences.
+    '''
+    seqio_in = sys.stdin
+    fh = ''
+    if not fasta:  # Check STDIN
+        logger.info('Parsing STDIN... Chunking reads in {}s...'.format(chunks))
+        process_filehandle(seqio_in, chunks, chunks_dir, gzip_me, byte_chunks)
+    else:  # Check FASTA
+        logger.info('Parsing FASTA file... Chunking reads in {}s...'.format(
+                                                                      chunks))
+        fh = return_filehandle(fasta)
+        process_filehandle(fh, chunks, chunks_dir, gzip_me, byte_chunks)
+
 
 if __name__ == '__main__':
     fasta = args.fasta
     chunk_dir = os.path.abspath(args.chunk_dir)
     gzip_me = args.gzip_output
+    byte_chunks = False
     if fasta:
         fasta = os.path.abspath(fasta)
     chunks = args.chunk_size
-    chunk_fasta(fasta, chunks, chunk_dir, gzip_me)
+    if args.chunk_bytes:
+        chunks = args.chunk_bytes
+        byte_chunks = True
+    chunk_fasta(fasta, chunks, chunk_dir, gzip_me, byte_chunks)
